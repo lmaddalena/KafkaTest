25ebdd74a69f123d915cb837fe63c5a568c27442
l.maddalena <l.maddalena@almaviva.it>
2020-02-26 15:34:37 +0100

    first commit

---
+++ producer/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Confluent.Kafka;
+
+
+namespace producer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kafka Producer");
+            Console.WriteLine("==============\n");
+
+            var conf = new ProducerConfig { BootstrapServers = "localhost:9092" };
+
+            Action<DeliveryReport<Null, string>> handler = r => 
+                Console.WriteLine(!r.Error.IsError
+                    ? $"Delivered message to {r.TopicPartitionOffset}"
+                    : $"Delivery Error: {r.Error.Reason}");
+
+            // If serializers are not specified, default serializers from
+            // `Confluent.Kafka.Serializers` will be automatically used where
+            // available. Note: by default strings are encoded as UTF8.
+            using (var p = new ProducerBuilder<Null, string>(conf).Build())
+            {
+                for (int i=0; i<100; ++i)
+                {
+                    p.Produce("my-topic", new Message<Null, string> { Value = i.ToString() }, handler);
+                }
+
+                // wait for up to 10 seconds for any inflight messages to be delivered.
+                p.Flush(TimeSpan.FromSeconds(10));
+            }
+
+
+        }
+    }
+}
